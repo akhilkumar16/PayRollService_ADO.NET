ba71952fc82cf47cf85ffa3b0b576092ba89f02d
Your Name <akhilkumarchandragiri@gmail.com>
2022-01-25 10:44:07 +0530

    [Akhil] connection of SQL

---
+++ EmployeePayrollService/EmployeeRepo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayrollService
+{
+    public class EmployeeRepo
+    {
+        public static string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Payroll_Service_ADO_NET;Integrated Security=True";
+
+        SqlConnection con = new SqlConnection(ConnectionString);
+        public bool AddEmployee(EmployeePayroll model)
+        {
+            try
+            {
+                using (this.con)
+                {
+                    SqlCommand command = new SqlCommand("SpAddEmployees", this.con);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmployeeId", model.EmployeeId);
+                    command.Parameters.AddWithValue("@EmployeeName", model.EmployeeName);
+                    command.Parameters.AddWithValue("@PhoneNumber", model.PhoneNumber);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@Department", model.Department);
+                    command.Parameters.AddWithValue("@Gender", model.Gender);
+                    command.Parameters.AddWithValue("@Deductions", model.Deductions);
+                    command.Parameters.AddWithValue("@TaxablePay", model.TaxablePay);
+                    command.Parameters.AddWithValue("@Tax", model.Tax);
+                    command.Parameters.AddWithValue("@NetPay", model.NetPay);
+                    command.Parameters.AddWithValue("@StartDate", model.StartDate);
+                    command.Parameters.AddWithValue("@EmployeeSalary", model.EmployeeSalary);
+                    command.Parameters.AddWithValue("@SalaryId", model.SalaryId);
+
+                    this.con.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.con.Close();
+                    if (result != 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.con.Close();
+            }
+        }
+    }
+}
---
+++ EmployeePayrollService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EmployeePayrollService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ado.net");
+            EmployeeRepo repo = new EmployeeRepo();
+            EmployeePayroll employee = new EmployeePayroll();
+
+            employee.EmployeeId = 1;
+            employee.EmployeeName = "a";
+            employee.PhoneNumber = 1234567899;
+            employee.Address = "telamgana";
+            employee.Department = " abc";
+            employee.Gender = 'M';
+            employee.Deductions = 200;
+            employee.TaxablePay = 250;
+            employee.Tax = 240;
+            employee.NetPay = 200000;
+            employee.StartDate = DateTime.Now;
+        }
+    }
+}
