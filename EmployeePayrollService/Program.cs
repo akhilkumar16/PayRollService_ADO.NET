a207af76a6ae0932a260d19300a08a21ffcae1de
Your Name <akhilkumarchandragiri@gmail.com>
2022-01-30 12:18:33 +0530

    updated

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -12,28 +12,28 @@ namespace EmployeePayrollService
 
                 EmployeeRepo repository = new EmployeeRepo();
                 //UC2
-             
-                repository.GetAllEmployeeData();              
+
+                repository.GetAllEmployeeData();
                 Console.WriteLine();
             }
             EmployeeRepo repository = new EmployeeRepo();
-                EmployeePayroll employee = new EmployeePayroll();
+            EmployeePayroll employee = new EmployeePayroll();
 
-                employee.EmployeeName = "a";
-                employee.PhoneNumber = 1234567899;
-                employee.Address = "telangana";
-                employee.Department = "abc";
-                employee.Gender = 'M';
-                employee.Deductions = 200;
-                employee.TaxablePay = 250;
-                employee.Tax = 240;
-                employee.NetPay = 200000;
-                employee.StartDate = DateTime.Now;
-                employee.City = "Varanasi";
-                employee.Country = "India";
+            employee.EmployeeName = "a";
+            employee.PhoneNumber = 1234567899;
+            employee.Address = "telangana";
+            employee.Department = "abc";
+            employee.Gender = 'M';
+            employee.Deductions = 200;
+            employee.TaxablePay = 250;
+            employee.Tax = 240;
+            employee.NetPay = 200000;
+            employee.StartDate = DateTime.Now;
+            employee.City = "HNk";
+            employee.Country = "India";
 
-                Console.WriteLine(repository.AddEmployee(employee) ? "Record Successfully Inserted On Table" : "Failed");
-            }
+            Console.WriteLine(repository.AddEmployee(employee) ? "Record Successfully Inserted On Table" : "Failed");
         }
     }
 }
+
